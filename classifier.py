c9ed611229c727f304952387b74d3b9d88642d0b
kishor1210 <kishorjha94@gmail.com>
2022-11-20 20:36:35 +0530

    code for final exam

--- classifier.py
+++ classifier.py
@@ -7,10 +7,11 @@ Created on Sun Nov 20 18:35:51 2022
 
 import pandas as pd
 import numpy as np
+import joblib
 import argparse
 	# Standard scientific Python imports
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score,precision_score,recall_score
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_digits
@@ -50,14 +51,18 @@ def build_classifier(args):
         model = GridSearchCV(DecisionTreeClassifier(), param_grid,refit = True)
         
     model.fit(X_train, y_train)
-        
+    filename = 'models/{model}'.format(model=args.clf_name+'_gamma=0.001_C=0.2.joblib')
+    joblib.dump(model, filename)
     y_pred = model.predict(X_test)
-    precision = precision_score(y_test, y_pred, average='weighted')
+    f1score = f1_score(y_test, y_pred, average='macro')
     recall = recall_score(y_test,y_pred, average='weighted')
     accuracy = accuracy_score(y_test, y_pred)
+    file = open('results/svm_45.txt', 'w')
+    file.write("Test accuracy:"+str(accuracy)+'\n')
+    file.write("Test macro-f1:"+str(f1score)+'\n')
 
         
-    return precision,recall,accuracy
+    return f1score,recall,accuracy
 
 
 if __name__ == '__main__':
@@ -70,7 +75,7 @@ if __name__ == '__main__':
     precision,recall,accuracy = build_classifier(args)
     
     print("Test accuracy:", accuracy)
-    print("Test precision:", precision)
+    print("Test f1 score:", precision)
     print("Test recall:", recall)
 
 
